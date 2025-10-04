9b3c94305b7e15f5f471f6a989e618a1016aa4ab
Interlights <nebuladust1@proton.me>
2025-10-03 20:41:11 -0400

    Update CustomerRecordsController.cs
    Added validation methods, and  added and refactored code

--- backend/controllers/CustomerRecordsController.cs
+++ backend/controllers/CustomerRecordsController.cs
@@ -18,112 +18,188 @@ namespace FalveyInsuranceGroup.Backend.Controllers
             _context = context;
         }
 
+        /// GET: api/customerrecords
+
+        /// <summary>
+        /// Gets a list of records
+        /// </summary>
+        /// <returns>A list of records</returns>
         [HttpGet]
-        public async Task<ActionResult<List<CustomerRecordDto>>> getCustomerRecords()
+        public async Task<ActionResult<List<CustomerRecordDto>>> getCustomerRecord()
         {
-            var customer_record = await _context.CustomerRecords
-            .Include(r => r.uploaded_by_employee)
+            var records = await _context.CustomerRecords
+            .AsNoTracking()
+            .Include(r => r.UploadedBy)
             .Select(r => new CustomerRecordDto
             {
-                record_id = r.record_id,
-                record_name = r.record_name,
+                document_id = r.document_id,
+                file_name = r.file_name,
                 url = r.url,
                 uploaded_by = r.uploaded_by,
-                uploaded_by_name = r.uploaded_by_employee != null ? r.uploaded_by_employee.name : null,
+                uploaded_by_name = r.UploadedBy != null ? r.UploadedBy.name : null,
                 uploaded_at = r.uploaded_at,
                 attached_to_type = r.attached_to_type,
-                attached_to_id = r.attached_to_id,
                 description = r.description
             }).ToListAsync();
 
 
-            return Ok(customer_record);
+            return Ok(records);
         }
 
+        /// GET: api/customerrecords/id
+
+        /// <summary>
+        /// Gets a record by ID
+        /// </summary>
+        /// <param name="id"> The id of the record to retrieve</param>
+        /// <returns>A customer record dto based on provided ID</returns>
+        /// <response code="200">Returns the record</response>
+        /// <response code="404">If the record is not found</response>
         [HttpGet("{id}")]
-        public async Task<ActionResult<CustomerRecordDto>> getCustomerRecord(int id)
+        public async Task<ActionResult<CustomerRecordDto>> getRecord(int id)
         {
-            var customer_record = await getCustomerRecordById(id);
+            var record = await _context.CustomerRecords
+            .AsNoTracking()
+            .Include(r => r.UploadedBy)
+            .FirstOrDefaultAsync(r => r.document_id == id);;
 
-            if (customer_record == null)
+            if (record == null)
             {
-                return NotFound();
+                return NotFound($"Record with ID {id} not found");
             }
 
-            return Ok(createCustomerRecordDto(customer_record));
+            return Ok(createCustomerRecordDto(record));
         }
 
+        /// POST: api/customerrecords
+
+        /// <summary>
+        /// Adds a new record
+        /// </summary>
+        /// <param name="record">Customer record object to add</param>
+        /// <returns>The new record</returns>
+        /// <response code="200">Record added successfully</response>
+        /// <response code="400">Validation failed</response>
         [HttpPost]
-        public async Task<ActionResult<CustomerRecordDto>> postCustomerRecord(CustomerRecord customer_record)
+        public async Task<ActionResult<CustomerRecordDto>> addRecord(CustomerRecord record)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
+            if (!ModelState.IsValid) {
+                return ValidationProblem(ModelState);
             }
 
-            _context.CustomerRecords.Add(customer_record);
+            if (record.document_id.HasValue) {
+                return BadRequest("Record ID should not be provided on creation.");
+            }
+
+            if (!hasValidRecordType(record.attached_to_type)) {
+                return BadRequest("Invalid type input");
+            }
+
+            _context.CustomerRecords.Add(record);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(getCustomerRecord), new { id = customer_record.record_id }, createCustomerRecordDto(customer_record));
+            return CreatedAtAction(nameof(getRecord), new { id = record.document_id }, createCustomerRecordDto(record));
         }
 
+        /// PUT: api/customerrecords/id
+
+        /// <summary>
+        ///  Updates an existing record by ID
+        /// </summary>
+        /// <param name="id">The record to update</param>
+        /// <param name="updated_record">Record object that holds the new details</param>
+        /// <returns>No content</returns>
+        /// <response code="204">The update was a success</response>
+        /// <response code="400">Validation failed</response>
+        /// <response code="404">Record not found</response>
         [HttpPut("{id}")]
-        public async Task<ActionResult> updateCustomerRecord(int id, CustomerRecord customer_record)
+        public async Task<ActionResult> updateRecord(int id, CustomerRecord updated_record)
         {
-            if (id != customer_record.record_id)
-            {
-                return BadRequest();
+            if (!ModelState.IsValid) {
+                return ValidationProblem(ModelState);
             }
 
-            _context.Entry(customer_record).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
+            if (!hasValidRecordType(updated_record.attached_to_type)) {
+                return BadRequest("Invalid type input");
+            }
 
+            var existing_record = await _context.CustomerRecords.FindAsync(id);
+
+            // Checks to see if customer record exists
+            if (existing_record == null) {
+                return NotFound($"Record with ID {id} not found");
+            }
+
+            // Updates fields
+            existing_record.file_name = updated_record.file_name;
+            existing_record.url = updated_record.url;
+            existing_record.uploaded_by = updated_record.uploaded_by;
+            existing_record.uploaded_at = updated_record.uploaded_at;
+            existing_record.attached_to_type = updated_record.attached_to_type;
+            existing_record.attached_to_id = updated_record.attached_to_id;
+            existing_record.description = updated_record.description;
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
+        /// DELETE: api/customerrecords/id
+
+        /// <summary>
+        /// Deletes an existing record
+        /// </summary>
+        /// <param name="id">The record to delete</param>
+        /// <returns>No content</returns>
+        /// <response code="204">Deletion was a success</response>
+        /// <response code="404">Record not found</response>
         [HttpDelete("{id}")]
-        public async Task<ActionResult> deleteCustomerRecord(int id)
+        public async Task<ActionResult> deleteRecord(int id)
         {
-            var customer_record = await getCustomerRecordById(id);
+            var record = await _context.CustomerRecords.FindAsync(id);
 
-            if (customer_record == null)
-            {
-                return NotFound();
+            if (record == null) {
+                return NotFound($"Record with ID {id} not found");
             }
 
-            _context.CustomerRecords.Remove(customer_record);
+            _context.CustomerRecords.Remove(record);
             await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
-
-
-        private async Task<CustomerRecord?> getCustomerRecordById(int id)
-        {
-            var customer_record = await _context.CustomerRecords
-            .Include(r => r.uploaded_by_employee)
-            .FirstOrDefaultAsync(r => r.record_id == id);
-
-            return customer_record != null ? customer_record : null;
-        }
-    
-
+        /// <summary>
+        /// Creates a dto using a CustomerRecord entity model
+        /// </summary>
+        /// <param name="r">The record entity model</param>
+        /// <returns>A customer record dto with the only necessary information</returns>
         private CustomerRecordDto createCustomerRecordDto(CustomerRecord r)
         {
             return new CustomerRecordDto
             {
-                record_id = r.record_id,
-                record_name = r.record_name,
+                document_id = r.document_id,
+                file_name = r.file_name,
                 url = r.url,
                 uploaded_by = r.uploaded_by,
-                uploaded_by_name = r.uploaded_by_employee != null ? r.uploaded_by_employee.name : null,
+                uploaded_by_name = r.UploadedBy != null ? r.UploadedBy.name : null,
                 uploaded_at = r.uploaded_at,
                 attached_to_type = r.attached_to_type,
-                attached_to_id = r.attached_to_id,
                 description = r.description
             };
+        }
+        
+        /// <summary>
+        /// Checks to see if a customer record object holds a valid record type
+        /// </summary>
+        /// <param name="record_type">The record type</param>
+        /// <returns>
+        ///     True - Has a valid record
+        ///     False - Has an invalid record type
+        /// </returns>
+        private bool hasValidRecordType(string status)
+        {
+            string[] allowed_types = { "Customer", "Policy", "Claim" };
+
+            return allowed_types.Contains(status);
         }
 
     }
