afdf6f336a8881de88154787f3a9a1b3e65c251f
Interlights <nebuladust1@proton.me>
2025-10-09 21:37:48 -0400

    Add files via upload

---
+++ backend/filters/RequiredNullAttribute.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FalveyProject.backend.filters
+{
+    public class RequiredNullAttribute : ValidationAttribute
+    {
+        public RequiredNullAttribute()
+        {
+            ErrorMessage = "ERROR: Field must not be given";
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+
+
+    }
+}
