699af81c8a2a74acefa6a871bd2b423636797d49
Interlights <nebuladust1@proton.me>
2025-10-09 21:42:28 -0400

    Update RequiredNullAttribute.cs

--- backend/filters/RequiredNullAttribute.cs
+++ backend/filters/RequiredNullAttribute.cs
@@ -1,6 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace FalveyInsuranceGroup.backend.filters
+namespace FalveyInsuranceGroup.Backend.Filters
 {
     public class RequiredNullAttribute : ValidationAttribute
     {
@@ -23,3 +23,4 @@ namespace FalveyInsuranceGroup.backend.filters
     }
 
 }
+
