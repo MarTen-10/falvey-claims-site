cdd5696235d469173dc79f1d7b19fe70df11f177
Interlights <nebuladust1@proton.me>
2025-10-09 22:19:01 -0400

    Update ClaimsController.cs

--- backend/controllers/ClaimsController.cs
+++ backend/controllers/ClaimsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FalveyInsuranceGroup.Db;
 using FalveyInsuranceGroup.Backend.Models;
+using FalveyInsuranceGroup.Backend.Helpers
 using FalveyInsuranceGroup.Backend.Dtos;
 using System.Linq.Expressions;
 
@@ -15,10 +16,13 @@ namespace FalveyInsuranceGroup.Backend.Controllers
     public class ClaimsController : ControllerBase
     {
         private readonly FalveyInsuranceGroupContext _context;
-
-        public ClaimsController(FalveyInsuranceGroupContext context)
+        private readonly InputService _service;
+        private static readonly  string[] ALLOWED_STATUS = { "Open", "Investigating", "Pending", "Approved", "Denied", "Closed" };
+        
+        public ClaimsController(FalveyInsuranceGroupContext context, InputService service)
         {
             _context = context;
+            _service = service;
         }
 
         /// GET: api/claims
@@ -55,9 +59,6 @@ namespace FalveyInsuranceGroup.Backend.Controllers
         {
             var claim = await _context.Claims
             .AsNoTracking()
-            .Include(c => c.claim_policy)
-            .Include(c => c.assigned_employee)
-            .Include(c => c.user_uploader)
             .FirstOrDefaultAsync(c => c.claim_id == id);
 
             if (claim == null)
@@ -65,7 +66,7 @@ namespace FalveyInsuranceGroup.Backend.Controllers
                 return NotFound($"Claim with ID {id} not found");
             }
 
-            return createClaimDto(claim);
+            return Ok( createClaimDto(claim) );
         }
 
         /// PUT: api/claims/id
@@ -83,26 +84,22 @@ namespace FalveyInsuranceGroup.Backend.Controllers
         public async Task<ActionResult> updateClaim(int id, [FromBody] ClaimDto dto)
         {
 
-            if (!hasValidStatus(dto.status))
-            {
+            if (!_service.hasValidEnumType(ALLOWED_STATUS, dto.status)) {
                 return BadRequest("Invalid status input");
             }
 
             // Ensures an existing policy is used
-            if (!await hasValidPolicy(dto.policy_id))
-            {
+            if (!await _service.hasValidPolicy(dto.policy_id)) {
                 return BadRequest("The given policy ID does not exist");
             }
 
-            if (await hasDuplicateClaimNumber(dto.claim_number))
-            {
+            if (await _service.hasDuplicateClaimNumber(dto.claim_number)) {
                 return BadRequest("The given claim number is already in use");
             }
 
             var claim = await _context.Claims.FindAsync(id);
 
-            if (claim == null)
-            {
+            if (claim == null) {
                 return NotFound($"Claim with ID {id} not found");
             }
 
@@ -122,6 +119,7 @@ namespace FalveyInsuranceGroup.Backend.Controllers
             return NoContent();
         }
 
+
         /// POST: api/claims
 
         /// <summary>
@@ -134,20 +132,17 @@ namespace FalveyInsuranceGroup.Backend.Controllers
         [HttpPost]
         public async Task<ActionResult> addClaim([FromBody] ClaimDto dto)
         {
-            // Ensures if a given email is unique
-            if (await hasDuplicateClaimNumber(dto.claim_number))
-            {
+            // Ensures if a given claim number is unique
+            if (await _service.hasDuplicateClaimNumber(dto.claim_number)) {
                 return BadRequest("The given claim number is already in use");
             }
-
-            // Ensures an existing policy is given
-            if (!await hasValidPolicy(dto.policy_id))
-            {
+             
+            // Ensures an existing policy is used
+            if (!await _service.hasValidPolicy(dto.policy_id)) {
                 return BadRequest("The given policy ID does not exist");
             }
 
-            if (!hasValidStatus(dto.status))
-            {
+            if (!_service.hasValidEnumType(ALLOWED_STATUS, dto.status)) {
                 return BadRequest("Invalid status input");
             }
 
@@ -155,7 +150,7 @@ namespace FalveyInsuranceGroup.Backend.Controllers
             {
                 policy_id = dto.policy_id,
                 claim_number = dto.claim_number,
-                status = dto.status ?? "Open",  // default 'Open' if null
+                status = dto.status,
                 date_of_loss = dto.date_of_loss,
                 date_reported = dto.date_reported,
                 reserve_amount = dto.reserve_amount,
@@ -223,8 +218,8 @@ namespace FalveyInsuranceGroup.Backend.Controllers
         }
 
         /// <summary>
-        /// Takes a Claim and maps it to a Claim dto
-        /// Then it represents it as data so it can be converted to a SQL query
+        /// Func - Takes a Claim and maps it to a Claim dto
+        /// Expression - Represents it as a expression tree so it can be converted to a SQL query
         /// </summary>
         private static Expression<Func<Claim, ClaimDto>> MapToClaimDto = c => new ClaimDto
         {
@@ -243,50 +238,9 @@ namespace FalveyInsuranceGroup.Backend.Controllers
             created_at = c.created_at
         };
 
-        /// <summary>
-        /// Checks to see if a claim object holds a valid status
-        /// </summary>
-        /// <param name="status">The status of a claim</param>
-        /// <returns>
-        ///     True - Has a valid status
-        ///     False - Has an invalid status
-        /// </returns>
-        private bool hasValidStatus(string status)
-        {
-            string[] allowed_status = { "Open", "Investigating", "Pending", "Approved", "Denied", "Closed" };
-
-            return allowed_status.Contains(status);
-        }
-
-        /// <summary>
-        /// Checks to see if a claim object holds a valid status
-        /// </summary>
-        /// <param name="status">The status of a claim</param>
-        /// <returns>
-        ///     True - Has a valid status
-        ///     False - Has an invalid status
-        /// </returns>
-
-        private async Task<bool> hasValidPolicy(int policy_id)
-        {
-            return await _context.Policies.AnyAsync(p => p.policy_id == policy_id);
-        }
-
-        /// <summary>
-        ///  Checks to see if a claim has a duplicate claim number
-        /// </summary>
-        /// <param name="claim_number">The claim number to check</param>
-        /// <returns>
-        ///     True - has a duplicate claim number
-        ///     False - Has a unique claim number
-        /// </returns>
-        private async Task<Boolean> hasDuplicateClaimNumber(string claim_number)
-        {
-            return await _context.Claims.AnyAsync(c => c.claim_number == claim_number);
-        }
-
 
     }
 
 
 }
+
